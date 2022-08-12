7373c3d2cd256329883ce084e1d8bb30a424b999
Aditya <adityaaryan.m4@gmail.com>
2022-08-12 08:07:52 +0530

    3rd commit

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -7,6 +7,11 @@ import LocalShippingIcon from '@mui/icons-material/LocalShipping';
 import InsertChartIcon from '@mui/icons-material/InsertChart';
 import NotificationsActiveIcon from '@mui/icons-material/NotificationsActive';
 import StoreMallDirectoryIcon from '@mui/icons-material/StoreMallDirectory';
+import SettingsApplicationsIcon from '@mui/icons-material/SettingsApplications';
+import SettingsSystemDaydreamOutlinedIcon from '@mui/icons-material/SettingsSystemDaydreamOutlined';
+import PsychologyOutlinedIcon from '@mui/icons-material/PsychologyOutlined';
+import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
+import ExitToAppOutlinedIcon from '@mui/icons-material/ExitToAppOutlined';
 
 const Sidebar = () => {
   return (
@@ -17,20 +22,30 @@ const Sidebar = () => {
       <hr />
       <div className="center">
         <ul>
-          <li><DashboardIcon /><span>Dashboard</span></li>
-          <li><PersonIcon /><span>Users</span></li>
-          <li><StoreMallDirectoryIcon /><span>Products</span></li>
-          <li><CreditCardIcon /><span>Orders</span></li>
-          <li><LocalShippingIcon /><span>Delivery</span></li>
-          <li><InsertChartIcon /><span>Stats</span></li>
-          <li><NotificationsActiveIcon /><span>Notifications</span></li>
-          <li><span>dashboard</span></li>
-          <li><span>dashboard</span></li>
-          <li><span>dashboard</span></li>
+          <p className="title">MAIN</p>
+          <li><DashboardIcon className='icon' /><span>Dashboard</span></li>
+          <p className="title">LISTS</p>
+          <li><PersonIcon className='icon' /><span>Users</span></li>
+          <li><StoreMallDirectoryIcon className='icon' /><span>Products</span></li>
+          <li><CreditCardIcon className='icon' /><span>Orders</span></li>
+          <li><LocalShippingIcon className='icon' /><span>Delivery</span></li>
+          <p className="title">USEFUL</p>
+          <li><InsertChartIcon className='icon' /><span>Stats</span></li>
+          <li><NotificationsActiveIcon className='icon' /><span>Notifications</span></li>
+          <p className="title">SERVICE</p>
+          <li><SettingsSystemDaydreamOutlinedIcon className='icon' /><span>System Health</span></li>
+          <li><PsychologyOutlinedIcon className='icon' /><span>Logs</span></li>
+          <li><SettingsApplicationsIcon className='icon' /><span>Settings</span></li>
+          <p className="title">USER</p>
+          <li><AccountCircleOutlinedIcon className='icon' /><span>Profile</span></li>
+          <li><ExitToAppOutlinedIcon className='icon' /><span>Logout</span></li>
 
         </ul>
       </div>
-      <div className="bottom">dark/light theme</div>
+      <div className="bottom">
+        <div className="colorOption"></div>
+        <div className="colorOption"></div>
+      </div>
     </div>
   )
 }
