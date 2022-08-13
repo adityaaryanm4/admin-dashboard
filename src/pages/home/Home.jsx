242b591fd984eedccaec58933ab5c4d3264be664
Aditya <adityaaryan.m4@gmail.com>
2022-08-14 01:09:23 +0530

    5th  commit

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,23 +1,35 @@
 import React from 'react'
+import FeaturedChart from '../../components/featuredChart/FeaturedChart'
+import ListContent from '../../components/listContent/ListContent'
 import Navbar from '../../components/navbar/Navbar'
 import Sidebar from '../../components/sidebar/Sidebar'
+import SimpleChart from '../../components/simpleChart/SimpleChart'
 import Widget from '../../components/widget/Widget'
 import "./home.scss"
 
 const Home = () => {
   return (
     <div className='home'>
-        <Sidebar />
-        <div className="homeContainer">
-          <Navbar />
-          <div className="widgets">
-            <Widget />
-            <Widget />
-            <Widget />
-            <Widget />
+      <Sidebar />
+      <div className="homeContainer">
+        <Navbar />
+        <div className="widgets">
+          <Widget type="user" />
+          <Widget type="order" />
+          <Widget type="earning" />
+          <Widget type="balance" />
+        </div>
+        <div className="charts">
+          <FeaturedChart />
+          <SimpleChart />
+        </div>
+        <div className="listContainer">
+          <div className="list">
+            <div className="listTitle">Latest Transactions</div>
+            <ListContent />
           </div>
-          home stuffs
-          </div>
+        </div>
+      </div>
     </div>
   )
 }
